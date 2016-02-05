c9494a21fa38abd634a71b6c2f8fa9897e20a949
ldd <leonardoldd@gmail.com>
2016-02-05 15:42:57 -0500

    first ios component: Main component
    updated index.ios.js to import Main.ios.js
    write a simple component in Main.js

--- index.ios.js
+++ index.ios.js
@@ -1,31 +1,15 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
 'use strict';
 import React, {
   AppRegistry,
   Component,
   StyleSheet,
-  Text,
-  View
 } from 'react-native';
+import Main from './app/components/Main';
 
 class SamosaSearch extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <Main style={styles.container}/>
     );
   }
 }
@@ -36,17 +20,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  }
 });
 
 AppRegistry.registerComponent('SamosaSearch', () => SamosaSearch);
