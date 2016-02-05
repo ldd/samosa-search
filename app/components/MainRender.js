7f38af58092d4c2fb6f3784a94f17a3c4c2b1204
ldd <leonardoldd@gmail.com>
2016-02-05 16:47:24 -0500

    Main: separation of rendering function and component
    Separate Main from its renderer so we can reuse it for the ios and android platforms
    (Main.js, MainRenderer.js)

---
+++ app/components/MainRender.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import {baseUtils} from '../base/base';
+import FloatingActionButton from 'material-ui/lib/floating-action-button';
+import ContentAdd from 'material-ui/lib/svg-icons/content/add';
+import NavigationBar from './NavigationBar/NavigationBarController';
+import ApplicationBar from './ApplicationBar';
+
+function MainRender(props, state){
+    let isLoggedIn = baseUtils.isLoggedIn();
+    let isRootRoute = props.location.pathname === '/';
+    return(
+        <div>
+            <ApplicationBar
+                isLoggedIn={isLoggedIn}
+                history={props.history}
+                handler={()=>this.setState({open: true})}
+            />
+            <NavigationBar
+                saleList={state.saleList}
+                history={props.history}
+                open={state.open}
+                closeHandler={()=> this.setState({open: false})}/>
+            {/*add the sale list as a property to the child*/}
+            {React.cloneElement(props.children, {saleList: state.saleList})}
+            {isLoggedIn && isRootRoute && <FloatingActionButton
+                style={{position: 'absolute', bottom:5, right:5}}
+                onClick={()=>{
+            this.setState({open: false});
+            props.history.pushState(null, '/sale')
+            }}>
+                <ContentAdd />
+            </FloatingActionButton>
+            }
+        </div>
+    )
+};
+
+export default MainRender;
