5e064479bc7c2fcf9c03f11f7a008a817f0e8661
ldd <leonardoldd@gmail.com>
2016-02-08 02:08:45 -0500

    further cleanup of utils.js (add reference)
    minor cleanup of utils.js to make it more compact,
    make sure there is a link reference to where this code was found
    explicitly mention that a firebase employee wrote this function
    (changed utils.js)

--- app/utils/utils.js
+++ app/utils/utils.js
@@ -1,15 +1,6 @@
-const utils = {
-};
-
 /**
- * Fancy ID generator that creates 20-character string identifiers with the following properties:
- *
- * 1. They're based on timestamp so that they sort *after* any existing ids.
- * 2. They contain 72-bits of random data after the timestamp so that IDs won't collide with other clients' IDs.
- * 3. They sort *lexicographically* (so the timestamp is converted to characters that will sort properly).
- * 4. They're monotonically increasing.  Even if you generate more than one in the same timestamp, the
- *    latter ones will sort after the former ones.  We do this by using the previous random bits
- *    but "incrementing" them by 1 (only in the case of a timestamp collision).
+ * firebase ID generator which can be found at
+ * https://gist.github.com/mikelehen/3596a30bd69384624c11
  */
 const generatePushID = (function() {
     // Modeled after base64 web-safe chars, but ordered by ASCII.
@@ -58,6 +49,5 @@ const generatePushID = (function() {
         return id;
     };
 })();
-utils.generateId = generatePushID;
-
+const utils = {generateId: generatePushID};
 export default utils;
